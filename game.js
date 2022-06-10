8890cefa75030aaeb79441493c950d7b46fc0db8
munchbyte <80370873+munchbyte@users.noreply.github.com>
2022-06-10 11:49:50 +0100

    final update

--- game.js
+++ game.js
@@ -1,32 +1,61 @@
 kaboom({
-    global:true,
+    global: true,
     fullscreen: true,
-    scale: 1,
+    scale: 2,
     debug: true,
     clearColor: [0, 0, 0, 1]
 })
+
+
+
 
 loadRoot('https://i.imgur.com/')
 loadSprite('coin','wbKxhcd.png')
 loadSprite('evil-shroom','KPO3fR9.png')
 loadSprite('brick','pogC9x5.png')
-loadSprite('block','bdrLpi6.png')
-loadSprite('mario','Wb1qfhK')
-loadSprite('mushroom','0wMd92p')
-loadSprite('surprise','gesQ1KP')
-loadSprite('unboxed','bdrLpi6')
-loadSprite('pipe-top-left','ReTPiWY')
-loadSprite('pipe-top-right','hj2GK4n')
-loadSprite('pipe-bottom-left','c1cYSbt')
-loadSprite('pipe-bottom-right','nqQ79eI')
-
-
+loadSprite('block','M6rwarW.png')
+loadSprite('mario','Wb1qfhK.png')
+loadSprite('mushroom','0wMd92p.png')
+loadSprite('surprise','gesQ1KP.png')
+loadSprite('unboxed','bdrLpi6.png')
+loadSprite('pipe-top-left','ReTPiWY.png')
+loadSprite('pipe-top-right','hj2GK4n.png')
+loadSprite('pipe-bottom-left','c1cYSbt.png')
+loadSprite('pipe-bottom-right','nqQ79eI.png')
 
 
 scene("game",() => {
    layers(['bg', 'obj', 'ui'], 'obj') 
+    
+   const map = [
+       '                                   ',
+       '                                   ',
+       '                                   ',
+       '                                   ',
+       '                                   ',
+       '      $                            ',
+       '                                   ',
+       '                      %*  -+        ',
+       '                          ()       ',
+       '============================  =====',
+       
+    ]
+    const levelCfg = {
+        width: 20,
+        height: 20,
+        '=': [sprite('brick'), solid()],
+        '$': [sprite('coin')],
+        '%': [sprite('surprise'), solid()],
+        '*': [sprite('surprise'), solid(), 'mushroom-surprise'],
+        '}': [sprite('unboxed'), solid()],
+        '(': [sprite('pipe-bottom-left'), solid()],
+        ')': [sprite('pipe-bottom-right'), solid()],
+        '-': [sprite('pipe-top-left'), solid()],
+        '+': [sprite('pipe-top-right'), solid()],
+    
+    }
 
-
+    const gameLevel = addLevel(map, levelCfg)
 })
 
-Start("game")
+start("game")
